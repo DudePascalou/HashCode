626e51d08f8ea6d0de55dabbef6d370941e0ecc4
François Descaves <fdescaves.stage@sodifrance.fr>
2018-03-01 20:09:08 +0100

    plein de choses !

--- 2018/HashCode/Ride.cs
+++ 2018/HashCode/Ride.cs
@@ -1,13 +1,25 @@
-﻿namespace HashCode
+﻿using CoperAlgoLib.Geometry;
+
+namespace HashCode
 {
     public class Ride
     {
+        public int Id { get; set; }
         public int StartingRow { get; set; }
         public int StartingColumn { get; set; }
         public int FinishingRow { get; set; }
         public int FinishingColumn { get; set; }
         public int EarliestStart { get; set; }
         public int LatestFinish { get; set; }
+
+        public Point StartingPoint { get; set; }
+        public Point FinishingPoint { get; set; }
+
+        public void InitPosition()
+        {
+            StartingPoint = new Point(StartingRow, StartingColumn);
+            FinishingPoint = new Point(FinishingColumn, FinishingColumn);
+        }
 
         public override string ToString()
         {
--- 2018/HashCode/Solver.cs
+++ 2018/HashCode/Solver.cs
@@ -10,21 +10,37 @@ namespace HashCode
     {
         public int Rows { get; set; }
         public int Columns { get; set; }
-        public int Vehicles { get; set; }
+        public int VehiclesCount { get; set; }
         public int RidesCount { get; set; }
         public int Bonus { get; set; }
         public int Steps { get; set; }
 
+        public ICollection<Vehicle> Vehicles { get; set; }
         public ICollection<Ride> Rides { get; set; }
 
         public Solver()
         {
-            Rides = new List<Ride>();         
+            Rides = new List<Ride>();
+            Vehicles = new List<Vehicle>();
         }
 
         public void Solve()
         {
-            
+
+            Vehicles.First().Rides.Add(new Ride() { Id = 0 });
+            Vehicles.Skip(1).First().Rides.Add(new Ride() { Id = 2 });
+            Vehicles.Skip(1).First().Rides.Add(new Ride() { Id = 1 });
+
+            // Output
+            foreach (var vehicle in Vehicles)
+            {
+                Console.Write($"{vehicle.Rides.Count}");
+                foreach (var ride in vehicle.Rides)
+                {
+                    Console.Write($" {ride.Id}");
+                }
+                Console.Write(Environment.NewLine);
+            }
         }
 
         public static int CalculMove(Point departure, Point arrival)
