2958fb81a554e5c36a3c9f544f22dad72182f4a8
Simon Beaulieu <sbeaulieu.stage@sodifrance.fr>
2019-02-28 21:03:36 +0100

    first ordered slideshow

--- 2019/HashCode/Solver.cs
+++ 2019/HashCode/Solver.cs
@@ -9,6 +9,7 @@ namespace HashCode
     {
         public int PhotoCount { get; set; }
         public IList<Photo> Photos { get; set; }
+        public IList<Slide> SlideList { get; set; }
         public IList<Slide> Slideshow { get; set; }
 
 
@@ -16,17 +17,37 @@ namespace HashCode
         {
             Photos = new List<Photo>();
             Slideshow = new List<Slide>();
+            SlideList = new List<Slide>();
+
         }
 
         public IEnumerable<string> Solve()
         {
-            foreach (var photo in Photos.Where(p => p.IsHorizontal))
+            IEnumerable<Photo> horizontals = Photos.Where(p => p.IsHorizontal);
+            foreach (var photo in horizontals)
             {
                 var slide = new Slide
                 {
-                    PhotoId = photo.Id
+                    PhotoId = photo.Id,
+                    Tags = photo.Tags
                 };
-                Slideshow.Add(slide);
+                SlideList.Add(slide);
+            }
+
+            
+            Random rd = new Random();
+            int starting = rd.Next(0, horizontals.Count());
+            Slideshow.Add(SlideList[starting]);
+            SlideList.Remove(SlideList[starting]);
+
+            Slide nextSlide = new Slide();
+            while (SlideList.Count != 0 && nextSlide!=null ) {
+             nextSlide = FindNextSlide(Slideshow.Last(), SlideList);
+                if (nextSlide != null)
+                {
+                    Slideshow.Add(nextSlide);
+                    SlideList.Remove(nextSlide);
+                }
             }
 
             yield return Slideshow.Count.ToString();
@@ -34,6 +55,34 @@ namespace HashCode
             {
                 yield return slide.ToString();
             }
+        }
+
+
+        private Slide FindNextSlide(Slide s, IList<Slide> slideList)
+        {
+            //IList<Slide> SOI = new List<Slide>();
+            int nbMaxTags = (int)Math.Floor(s.Tags.Count / 2d);
+            Slide sli = null;
+            int nbTagsInterest = 0;
+            foreach (var slide in SlideList)
+            {
+        
+                var nbCommonTags = MatchingTags(slide.Tags, s.Tags).Count;
+                if (nbCommonTags > nbTagsInterest)
+                {
+                    sli = slide;
+                    nbTagsInterest = nbCommonTags;
+                }
+
+                //if (nbCommonTags > 0 && nbCommonTags < nbMaxTags)
+                //{
+                //    SOI.Add(slide);
+                //}
+            }
+            return sli;
+            //var query = from s1 in slideList
+            //            where MatchingTags(s1.Tags, s.Tags).Count <= nbMaxTags && MatchingTags(s1.Tags, s.Tags).Count > 0
+            //            select s;
         }
 
 
