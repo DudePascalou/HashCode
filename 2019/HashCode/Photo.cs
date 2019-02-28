3889cf894ac28cd177747921fbf1b1e4ffdaa837
Pascal Crouzet <pcrouzet@sodifrance.fr>
2019-02-28 19:24:24 +0100

    Parse input.

---
+++ 2019/HashCode/Photo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace HashCode
+{
+    public class Photo
+    {
+        public bool IsHorizontal { get; set; }
+        public bool IsVertical { get; set; }
+        public int TagCount { get; set; }
+        public IList<string> Tags { get; set; }
+
+        public Photo()
+        {
+            Tags = new List<string>();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1} {2}", IsHorizontal ? "H" : "V", TagCount, string.Join(" ", Tags));
+        }
+    }
+}
